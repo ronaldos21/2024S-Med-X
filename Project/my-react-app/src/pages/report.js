e01d30392057438d2b047c247fec72065a06a8f0
ChrisOspina1 <66086599+ChrisOspina@users.noreply.github.com>
2024-04-02 13:57:05 -0400

    added print report button

--- Project/my-react-app/src/pages/report.js
+++ Project/my-react-app/src/pages/report.js
@@ -1,10 +1,49 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Page, Text, View, Document, StyleSheet, Image} from '@react-pdf/renderer';
+
+
+//settign styles for the pdf document
+const styles = StyleSheet.create({
+  page:{
+    flexDirection: 'column',
+    backgroundColor: '#ffff',
+    padding: 10,
+  },
+
+  section:{
+    margin:10,
+    padding:10,
+    flexGrow:1
+  },
+  image:{
+    width:'100%',
+    height:'auto',
+  }
+});
+
+//For rendering the pdf
+const MyDocument = ({result, url, formattedDate} )=>(
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Result: {result}</Text>
+        <Text>Date: {formattedDate}</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Image:</Text>
+        <Image style={styles.image}src={url} />
+      </View>
+    </Page>
+  </Document>
+);
+
+
 const Report = () => {
   const location = useLocation();
-  const result = location.state.result;
-  const url = location.state.img;
-
+  const result = location.state ? location.state.result: "" ;
+  const url = location.state? location.state.img: "";
+  
 
   const currentDate = new Date();
 
@@ -16,7 +55,23 @@ const Report = () => {
     hour: 'numeric',
     minute: 'numeric',
     hour12: true,
-  });
+  }
+  );
+
+  //to handle sumbission to pdf file
+    const handleSubmit = () =>{
+
+      const blob = new Blob([<MyDocument result={result} url={url} formattedDate={formattedDate}/>],{type:"application/pdf"});
+
+      const newLink =  window.URL.createObjectURL(blob);
+      const newWindow = window.open(newLink);
+
+      newWindow.print();
+
+      console.log(result);
+      console.log(url);
+      console.log(formattedDate);
+  };
   return (
 
     <div
@@ -49,8 +104,13 @@ const Report = () => {
           <div className="self-stretch  flex-col justify-start items-start  flex">
             <div
               className="text-center text-customPurple  text-3xl font-normal font-['Inter']">{result}</div>
+          </div >
+          <div className="group ">
+           <button className="group-hover:bg-slate-700 px-5 py-2.5 text-primary bg-white bg-opacity-80 rounded-[5px] justify-start items-start gap-2.5 inline-flex active:bg-green-700 focus:ring focus:ring-gray-700" onClick={handleSubmit}>
+           <div className='group-hover:text-white'>Print Report</div>
+           </button>
           </div>
-
+           
         </div>
       </div>
     </div>
