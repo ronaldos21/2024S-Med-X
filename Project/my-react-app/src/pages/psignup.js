af1bd3e099467c9be242e024709db2c7ca1101b4
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-04-19 12:45:30 -0400

    removed unnecessary imports

--- Project/my-react-app/src/pages/psignup.js
+++ Project/my-react-app/src/pages/psignup.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../components/img/Logo.png';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
