3c2640ac747d2e06c92d68bcc24ba3bfdc62a185
Marla <marcapistran@gmail.com>
2024-04-24 11:22:44 -0400

    Updated Doctor's Portal ReportPage UI to reflect the format of the Dashboard UI.

--- Project/my-react-app/src/pages/reportpage.js
+++ Project/my-react-app/src/pages/reportpage.js
@@ -87,12 +87,11 @@ const ReportPage = () => {
 
     if (userType === "patient") {
         return (
-            
             <div className='flex h-full items-start flex-col'>
 
                 <div className="Report text-white text-4xl p-5 justify-start">Results</div>
 
-                <div className='flex    flex-row p-5 gap-5'>
+                <div className='flex flex-row p-5 gap-5'>
                     <div
                         className="w-[200px] h-[300px] rounded-[20px] flex-col justify-center items-start  flex">
                         <img
@@ -152,100 +151,174 @@ const ReportPage = () => {
                 </div>
               
             </div>
-
         );
     } else if (userType === "doctor") {
         return (
-            <div
-                className="w-full h-full pl-[30px] pr-[15px] pt-[15px] pb-[30px] justify-start items-start  inline-flex"
-                key={refreshKey}>
-                <div
-                    className="self-stretch p-5 flex-col justify-start items-start  inline-flex">
-                    <div
-                        className="Report text-center text-white text-4xl  font-normal font-['Inter']">Results</div>
-                    <div
+            <div className='flex h-full items-start flex-col'>
+                <div className="Report text-white text-4xl p-5 justify-start">Results</div>
+                <div className="flex flex-row p-5 gap-5">
+                    <div 
                         className="w-[200px] h-[300px] rounded-[20px] flex-col justify-center items-start  flex">
                         <img
                             className="rounded-[10px] object-cover w-full h-full"
                             src={report.data.xr_image}
                             alt="Report_image"/>
                     </div>
-                </div>
-                <div
-                    className="grow shrink basis-0 self-stretch flex-col justify-center items-center  inline-flex">
-                    <div className="self-stretch p-5 flex-col justify-center items-center  flex">
-                        <div className="self-stretch h-12 justify-between items-center inline-flex">
-                            <div className="text-center text-white text-3xl font-normal font-['Inter']">Report ID: #{report.id}</div>
-                            <div className="text-customGreen font-normal font-['Inter']">
-                                {
-                                    report
-                                        .data
-                                        .scan_date
-                                        .toDate()
-                                        .toLocaleString()
-                                }
-
-                            </div>
+                    <div className='flex-col flex gap-5'>
+                        <div className='flex flex-row gap-5 items-center justify-between'>
+                                <div className="text-center text-white text-3xl">Report ID: #{report.id}</div>
+                                <div className="text-customGreen font-normal">{
+                                        report
+                                            .data
+                                            .scan_date
+                                            .toDate()
+                                            .toLocaleString()
+                                }</div>
                         </div>
-                        <div className="self-stretch h-full flex-col justify-start items-start  flex">
-                            <div
-                                className="text-center text-customPurple text-xl font-normal font-['Inter']">
+                        <div className='flex flex-col justify-start items-start '>
+                            <div className='text-customPurple text-xl'>
                                 {report.data.medical_term}
                             </div>
-                            <div
-                                className="self-stretch text-justify text-white font-normal font-['Inter'] text-wrap w-1/2">
+
+                            <div className='text-white'>
                                 {report.data.medical_description}
                             </div>
                         </div>
-                    </div>
-                    <div className="self-stretch h-full flex-col justify-center items-end  flex">
-                        <div className="self-stretch justify-start items-center  inline-flex">
-                            <div className="text-center text-white text-xl font-normal font-['Inter']">Report status:</div>
-                            <StatusButton status={report.data.status}/>
-                        </div>
-                    </div>
-                    <div className="self-stretch h-full flex-col justify-center items-end  flex">
-                        <div className="self-stretch h-full flex-col justify-center items-start  flex">
-                            <div className="text-center text-white text-xl font-normal font-['Inter']">Your Comment:</div>
-                            <div
-                                className="self-stretch h-full p-2.5 bg-neutral-900 rounded-[20px] flex-col justify-center items-center gap-2.5 flex">
-                                <div
-                                    className="self-stretch grow shrink basis-0 text-justify text-white text-2xl font-normal font-['Inter']">
-                                    <form onSubmit={(e) => handleSubmit(e, report.id, comment)}>
-                                        <label
-                                            htmlFor="comment"
-                                            className="block text-white text-xl font-normal font-['Inter']">Medical Practitioner's comment:</label>
-                                        <textarea
-                                            id="comment"
-                                            className="w-full bg-neutral-800 text-white rounded p-2"
-                                            rows="4"
-                                            value={comment}
-                                            onChange={(e) => setComment(e.target.value)}
-                                            placeholder="Enter your comment here..."></textarea>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="justify-end items-center  inline-flex">
+                        <div className='flex flex-row gap-5 items-center'>
+                            <div className='text-white '>Report Status:</div>
 
-                            <PrintableReport
-                                result={report.data.medical_term}
-                                url={report.data.xr_image}
-                                nextReportId={report.id}
-                                formattedDate={report.data.scan_date}
-                                comment={report.data.mp_comment}
-                                pname={report.data.patientName}/>
-                            <div className='group'>
-                                <button
-                                    className="group-hover:bg-slate-700 px-5 py-2.5 text-primary bg-white bg-opacity-80 rounded-[5px] justify-start items-start gap-2.5 inline-flex active:bg-green-700 focus:ring focus:ring-gray-700"
-                                    onClick={(e) => handleSubmit(e, report.id, comment)}>
-                                    <div className='group-hover:text-white'>Update Report</div>
-                                </button>
-                            </div>
+                            <StatusButton status={report.data.status}/>
+
+                        </div>
+                        <div className='flex flex-col text-white items-start gap-2'>
+                                <div>Medical Practitioner's Comment:</div>
+                                <div className="flex self-stretch p-4 bg-neutral-900 rounded flex-col">
+                                    <div
+                                        className="text-white text-l">
+                                        <form onSubmit={(e) => handleSubmit(e, report.id, comment)}>
+                                         <label
+                                            htmlFor="comment">
+                                         </label>
+                                            <textarea
+                                                id="comment"
+                                                className="w-full bg-neutral-800 text-white rounded p-3"
+                                                rows="4"
+                                                value={comment}
+                                                onChange={(e) => setComment(e.target.value)}
+                                                placeholder="Enter your comment here...">
+                                            </textarea>
+                                        </form>
+                                    </div>
+                                </div>
                         </div>
                     </div>
                 </div>
+                <div className='flex w-full justify-end items-end pr-5 h-full'>
+                <div >
+                        <PrintableReport
+                            result={report.data.medical_term}
+                            url={report.data.xr_image}
+                            nextReportId={report.id}
+                            formattedDate={report.data.scan_date}
+                            comment={report.data.mp_comment}
+                            pname={report.data.patientName}
+                            desc={report.data.medical_description}
+                            />
+                    </div>
+                </div>
             </div>
+            
+           // <div
+             //   className="w-full h-full pl-[30px] pr-[15px] pt-[15px] pb-[30px] justify-start items-start  inline-flex"
+               // key={refreshKey}>
+               // <div
+                 //   className="self-stretch p-5 flex-col justify-start items-start  inline-flex">
+                   // <div
+                     //   className="Report text-center text-white text-4xl  font-normal font-['Inter']">Results</div>
+                   // <div
+                     //   className="w-[200px] h-[300px] rounded-[20px] flex-col justify-center items-start  flex">
+                       // <img
+                         //   className="rounded-[10px] object-cover w-full h-full"
+                           // src={report.data.xr_image}
+                           // alt="Report_image"/>
+                   // </div>
+               // </div>
+               // <div
+                 //   className="grow shrink basis-0 self-stretch flex-col justify-center items-center  inline-flex">
+                   // <div className="self-stretch p-5 flex-col justify-center items-center  flex">
+                     //   <div className="self-stretch h-12 justify-between items-center inline-flex">
+                       //     <div className="text-center text-white text-3xl font-normal font-['Inter']">Report ID: #{report.id}</div>
+                         //   <div className="text-customGreen font-normal font-['Inter']">
+                           //     {
+                             //       report
+                               //         .data
+                                 //       .scan_date
+                                   //     .toDate()
+                                     //   .toLocaleString()
+                               // }
+
+                           // </div>
+                       // </div>
+                       // <div className="self-stretch h-full flex-col justify-start items-start  flex">
+                         //   <div
+                           //     className="text-center text-customPurple text-xl font-normal font-['Inter']">
+                             //   {report.data.medical_term}
+                           // </div>
+                           // <div
+                             //   className="self-stretch text-justify text-white font-normal font-['Inter'] text-wrap w-1/2">
+                               // {report.data.medical_description}
+                            // </div>
+                       // </div>
+                   // </div>
+                   // <div className="self-stretch h-full flex-col justify-center items-end  flex">
+                     //   <div className="self-stretch justify-start items-center  inline-flex">
+                        //    <div className="text-center text-white text-xl font-normal font-['Inter']">Report status:</div>
+                          //  <StatusButton status={report.data.status}/>
+                      //  </div>
+                  //  </div>
+                  //  <div className="self-stretch h-full flex-col justify-center items-end  flex">
+                    //    <div className="self-stretch h-full flex-col justify-center items-start  flex">
+                        //    <div className="text-center text-white text-xl font-normal font-['Inter']">Your Comment:</div>
+                          //  <div
+                            //    className="self-stretch h-full p-2.5 bg-neutral-900 rounded-[20px] flex-col justify-center items-center gap-2.5 flex">
+                              //  <div
+                                //    className="self-stretch grow shrink basis-0 text-justify text-white text-2xl font-normal font-['Inter']">
+                                  //  <form onSubmit={(e) => handleSubmit(e, report.id, comment)}>
+                                    //    <label
+                                      //      htmlFor="comment"
+                                        //    className="block text-white text-xl font-normal font-['Inter']">Medical Practitioner's comment:</label>
+                                    //    <textarea
+                                      //      id="comment"
+                                        //    className="w-full bg-neutral-800 text-white rounded p-2"
+                                          //  rows="4"
+                                          //  value={comment}
+                                          //  onChange={(e) => setComment(e.target.value)}
+                                          //  placeholder="Enter your comment here..."></textarea>
+                                 //   </form>
+                             //   </div>
+                        //    </div>
+                    //    </div>
+                      //  <div className="justify-end items-center  inline-flex">
+
+                        //    <PrintableReport
+                          //      result={report.data.medical_term}
+                            //    url={report.data.xr_image}
+                              //  nextReportId={report.id}
+                               // formattedDate={report.data.scan_date}
+                               // comment={report.data.mp_comment}
+                               // pname={report.data.patientName}/>
+                         //   <div className='group'>
+                           //     <button
+                             //       className="group-hover:bg-slate-700 px-5 py-2.5 text-primary bg-white bg-opacity-80 rounded-[5px] justify-start items-start gap-2.5 inline-flex active:bg-green-700 focus:ring focus:ring-gray-700"
+                               //     onClick={(e) => handleSubmit(e, report.id, comment)}>
+                                 //   <div className='group-hover:text-white'>Update Report</div>
+                            //   </button>
+                          //  </div>
+                       // </div>
+               //     </div>
+             //   </div>
+           // </div>
+                            
         );
     } else {
         return null;
